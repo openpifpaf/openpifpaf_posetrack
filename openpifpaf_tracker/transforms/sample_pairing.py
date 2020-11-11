f3c2e92a31bd39d2edc9d2b2aed4f08081020115
Sven Kreiss <me@svenkreiss.com>
2020-11-11 10:00:39 +0100

    SamplePairing

--- openpifpaf_tracker/transforms/sample_pairing.py
+++ openpifpaf_tracker/transforms/sample_pairing.py
@@ -15,14 +15,15 @@ class SamplePairing(openpifpaf.transforms.Preprocess):
         self.previous_all_annotations = []
 
     def __call__(self, original_images, original_all_anns, metas):
-        images = copy.deepcopy(original_images)
+        images = original_images
         all_anns = copy.deepcopy(original_all_anns)
 
         if self.previous_images is not None:
             # image
-            for current_image, previous_image in zip(images, self.previous_images):
-                current_image *= 0.5
-                current_image += 0.5 * previous_image
+            images = [
+                PIL.Image.blend(current_image, previous_image, 0.5)
+                for current_image, previous_image in zip(images, self.previous_images)
+            ]
 
             # annotations
             for current_anns, previous_anns in zip(all_anns, self.previous_all_annotations):
